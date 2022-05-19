9e8966d309c0be29b7384a34af04230f4fa33578
Anna <APodoynitsyna@smartspaceplc.com>
2022-05-19 12:00:14 +0300

    test

--- SRComponent/src/search/search.tsx
+++ SRComponent/src/search/search.tsx
@@ -3,9 +3,9 @@ import { FC } from 'react'
 export const Search: FC = () => {
   return (
     <>
-  <div>Search</div>
+  <div>Search</div>      
 
-               <div>Text</div>
+          <div>Text</div>
   </>
      )    
 }
